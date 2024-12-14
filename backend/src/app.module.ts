0221ffd77f6eba9fb2bc446f9c09a7cc88874e3e
hudsonsouza99 <hudson.developer@protonmail.com>
2024-12-14 06:25:18 +0000

    feat(backend): initial NestJS + GraphQL configuration

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+  ],
+  providers: [AppResolver],
+})
+export class AppModule {}
